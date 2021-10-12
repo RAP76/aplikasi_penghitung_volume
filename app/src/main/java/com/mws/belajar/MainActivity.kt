ba62c233dc167cbc028db10926b8c6b4932b16bd
RAP76 <rafiajip01@gmail.com>
2021-10-12 22:08:17 +0700

    Initial commit

---
+++ app/src/main/java/com/mws/belajar/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.mws.belajar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var txtPanjang : EditText
+    private lateinit var txtLebar : EditText
+    private lateinit var txtTinggi : EditText
+    private lateinit var btnHitung : Button
+    private lateinit var txtResult : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txtPanjang = findViewById(R.id.txtPanjang)
+        txtLebar = findViewById(R.id.txtLebar)
+        txtResult = findViewById(R.id.txtResult)
+        txtTinggi = findViewById(R.id.txtTinggi)
+        btnHitung = findViewById(R.id.btnHitung)
+
+        btnHitung.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View) {
+        if(p0.id == R.id.btnHitung) {
+            val inputPanjang = txtPanjang.text.toString().trim()
+            val inputLebar = txtLebar.text.toString().trim()
+            val inputTinggi = txtTinggi.text.toString().trim()
+
+            val volume = inputPanjang.toDouble() * inputLebar.toDouble() * inputTinggi.toDouble()
+
+            txtResult.text = volume.toString()
+
+
+        }
+    }
+}
+
